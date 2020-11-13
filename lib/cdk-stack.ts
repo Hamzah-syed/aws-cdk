5372b90be008c7e4afe39d2644f7a37bc8708333
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-11-13 10:37:44 +0500

    hello world api created

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,11 +1,23 @@
 import * as cdk from '@aws-cdk/core';
-import * as s3 from "@aws-cdk/aws-s3"
+import * as lambda from "@aws-cdk/aws-lambda"
+import * as apigw from "@aws-cdk/aws-apigateway"
+
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'myBucket', {
-      versioned:true
+    const hello = new lambda.Function(this,"helloWorld", {
+      //our function uses nodejs_10_X
+      runtime:lambda.Runtime.NODEJS_10_X,
+      // the handler code is loaded from lambda directory which is created in root
+      code:lambda.Code.fromAsset('lambda'),
+      // hello is the file name and handler is exported from that file 
+      handler: 'hello.handler'
     })
+
+    new apigw.LambdaRestApi(this,"endpoint",{
+      handler:hello
+    })
+   
   }
 }
